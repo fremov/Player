b7187c9f390315171d47192c6627570be2c25e38
fremov <kor7781@gmail.com>
2023-05-01 13:56:25 +0300

    fix 1

--- src/script.js
+++ src/script.js
@@ -38,6 +38,15 @@ class Actor {
     set_weapon(weapon) {
         this._weapon = weapon;
     }
+    set_armor_set(armor) {
+        this._armor_set = armor;
+    }
+    set_ring(ring) {
+        this._ring = ring;
+    }
+    set_amulet(amulet) {
+        this._amulet = amulet;
+    }
     moveTo(x, y, z) {
         console.log(`x: ${x}\ny: ${y}\nz: ${z}`)
     }
@@ -47,14 +56,18 @@ class Actor {
     }
 
     addSpell(spell_title) {
-        this._spells_list.push(spell_title);
+        if (!this._spells_list.includes(spell_title)) {
+            this._spells_list.push(spell_title);
+        }
         this.updateView();
         return this._spells_list;
     }
 
 
     add_perks(perk_title) {
-        this._perks_list.push(perk_title);
+        if (!this._perks_list.includes(perk_title)) {
+            this._perks_list.push(perk_title);
+        }
         this.updateView();
         return this._perks_list;
     }
@@ -83,6 +96,8 @@ class Actor {
         document.getElementById("get_spells").textContent = `Заклинания: ${this._spells_list}`;
         document.getElementById("get_weapon").textContent = `Оружие: ${this._weapon}`;
         document.getElementById("get_armor_set").textContent = `Сет брони: ${this._armor_set}`;
+        document.getElementById("get_amulet").textContent = `Амулет: ${this._amulet}`;
+        document.getElementById("get_ring").textContent = `Кольцо: ${this._ring}`;
     }
 
 }
@@ -107,20 +122,32 @@ loadSelectData('set_weapon', 'src/json/data.json');
 loadSelectData('set_perks', 'src/json/perks.json');
 // для спелов
 loadSelectData('set_spells', 'src/json/all_spells.json');
+// для брони
+loadSelectData('set_armor_set', 'src/json/data.json');
+// для колец
+loadSelectData('set_ring', 'src/json/data.json');
+// для амлулетов
+loadSelectData('set_amulet', 'src/json/data.json');
 
 
 let race = document.getElementById('race');
 let spells = document.getElementById('set_spells');
 let perks = document.getElementById('set_perks');
 let weapon = document.getElementById('set_weapon');
+let armor_set = document.getElementById('set_armor_set');
+let amulet = document.getElementById('set_amulet');
+let ring = document.getElementById('set_ring');
 let button = document.getElementById('add_to_actor');
-// let create_actor = document.getElementById('Actor');
+let Actor_name = document.getElementById('Actor_name');
 
 
-let player = new Actor(0, 0, 'Выберите расу', "Выберите амулет", 'Выберите набор брони', 'Выберите кольцо');
+let player = new Actor(0, 0, 'Выберите расу', "Выберите амулет", 'Выберите набор брони', 'Выберите кольцо', 'Выберите оружие');
 
 button.addEventListener('click', function () {
-    player.set_weapon(weapon.value)
+    player.set_weapon(weapon.value);
+    player.set_ring(ring.value);
+    player.set_amulet(amulet.value);
+    player.set_armor_set(armor_set.value);
     player.set_race(race.value);
     player.add_perks(perks.value);
     player.addSpell(spells.value);
